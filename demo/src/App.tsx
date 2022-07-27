51e16ef0dbca4ef214c580bb31f88d955514599a
LitIpfx <110101229+LitIpfx@users.noreply.github.com>
2022-07-28 07:49:20 +0800

    clean up

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     particleLimit: 20,
     zIndexStart: 5,
     onDamage: (pageHealth: number) => console.log(pageHealth),
-    pageHealth: 1,
+    pageHealth: 200,
     volumeChangeDelta: 0.5,
   };
 
@@ -27,29 +27,6 @@ function App() {
   }, [destroyer]);
 
   return <div className="App myParent" ref={(el) => (myParent = el)} />;
-
-  // const [count, setCount] = useState(0);
-
-  // return (
-  //   <div className="App">
-  //     <div>
-  //       <a href="https://vitejs.dev" target="_blank">
-  //         <img src="/vite.svg" className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://reactjs.org" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-  //   </div>
-  // );
 }
 
 export default App;
