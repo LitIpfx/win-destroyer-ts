0b4afd2ef7c6a95e2299cb4920581b6def7d0e2b
LitIpfx <110101229+LitIpfx@users.noreply.github.com>
2022-08-01 12:13:36 +0800

    update: use loaded sounds from constructor Destroyer

--- src/weaponFactory.ts
+++ src/weaponFactory.ts
@@ -3,15 +3,22 @@ import sprites from './sprites';
 import sounds from './sounds';
 import weaponData from './weapons';
 import { Game } from './interfaces';
-import { WeaponOptions } from './types';
+import { Sounds, WeaponOptions } from './types';
 
 /** Compiles a list of weapon objects */
-export const weaponFactory = (game: Game) => {
+export const weaponFactory = (game: Game, loadedSounds: Sounds) => {
   const weaponsList = [] as Weapon[];
 
   for (const name of Object.keys(weaponData)) {
     const n = name as keyof typeof weaponData;
-    weaponsList.push(new Weapon(game, sounds[n], sprites[n], weaponData[n] as unknown as WeaponOptions));
+    weaponsList.push(
+      new Weapon(
+        game,
+        loadedSounds[n]?.length > 0 ? loadedSounds[n] : sounds[n],
+        sprites[n],
+        weaponData[n] as unknown as WeaponOptions
+      )
+    );
   }
   return weaponsList;
 };
